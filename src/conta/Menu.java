9d5a9a5d3cbfd45d6ee23394453b550bb72bb371
Hiago Oliveira <hiagosantos223@gmail.com>
2024-03-07 14:32:57 -0300

    Task 3 entregue

--- src/conta/Menu.java
+++ src/conta/Menu.java
@@ -3,6 +3,8 @@ package conta;
 import java.util.Scanner;
 
 import conta.model.Conta;
+import conta.model.ContaCorrente;
+import conta.model.ContaPoupanca;
 import conta.util.Cores;
 
 public class Menu {
@@ -11,13 +13,29 @@ public class Menu {
 		
 		// Testes da Classe Conta
 		
-		Conta c1 = new Conta(2442, 29, 2, "Hiago dos Santos", 12500f);
-		
-		c1.visualizar();
-		c1.sacar(15000.0f);
-		c1.visualizar();
-		c1.depositar(5000.0f);
-		c1.visualizar();
+		// Teste da Classe Conta
+				Conta c1 = new Conta(3, 123, 1, "Mariana", 500000.0f);
+				c1.visualizar();
+				c1.sacar(12000.0f);
+				c1.visualizar();
+				c1.depositar(5000.0f);
+				c1.visualizar();
+		        
+				// Teste da Classe Conta Corrente
+				ContaCorrente cc1 = new ContaCorrente(1, 123, 1, "José da Silva", 0.0f, 1000.0f);
+				cc1.visualizar();
+				cc1.sacar(12000.0f);
+				cc1.visualizar();
+				cc1.depositar(5000.0f);
+				cc1.visualizar();
+				
+		        // Teste da Classe Conta Poupança
+				ContaPoupanca cp1 = new ContaPoupanca(2, 123, 2, "Maria dos Santos", 100000.0f, 15);
+				cp1.visualizar();
+		        cp1.sacar(1000.0f);
+				cp1.visualizar();
+				cp1.depositar(5000.0f);
+				cp1.visualizar();
 		
 		
 		
---
+++ src/conta/model/ContaCorrente.java
@@ -0,0 +1,43 @@
+package conta.model;
+
+
+// Conta Corrente herda de Conta
+public class ContaCorrente extends Conta {
+	
+	private float limite;
+
+	public ContaCorrente(int numero, int agencia, int tipo, String titular, float saldo, float limite) {
+		
+		//Super Classe: representa o construtor da Classe principal(Conta)
+		super(numero, agencia, tipo, titular, saldo);
+		
+		this.limite = limite;
+		
+	}
+
+	public float getLimite() {
+		return limite;
+	}
+
+	public void setLimite(float limite) {
+		this.limite = limite;
+	}
+	
+	@Override
+	public boolean sacar(float valor) {
+		
+		if(this.getSaldo() + this.getLimite() < valor) {
+			System.out.println("\n Saldo Insulficiente!");
+			return false;
+		}
+		this.setSaldo(this.getSaldo() - valor);
+		return true;
+	}
+	
+	@Override
+	public void visualizar() {
+		super.visualizar();
+		System.out.println("Limite de Crédito: " + this.limite);
+	}
+	
+}

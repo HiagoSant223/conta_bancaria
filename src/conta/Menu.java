58e23153814eada4e58da10b73c371d38cd53775
Hiago Oliveira <hiagosantos223@gmail.com>
2024-03-07 17:08:55 -0300

    Update Classe Controller

--- src/conta/Menu.java
+++ src/conta/Menu.java
@@ -13,13 +13,7 @@ public class Menu {
 		
 		// Testes da Classe Conta
 		
-		// Teste da Classe Conta
-				Conta c1 = new Conta(3, 123, 1, "Mariana", 500000.0f);
-				c1.visualizar();
-				c1.sacar(12000.0f);
-				c1.visualizar();
-				c1.depositar(5000.0f);
-				c1.visualizar();
+		// Teste da Classe Cont
 		        
 				// Teste da Classe Conta Corrente
 				ContaCorrente cc1 = new ContaCorrente(1, 123, 1, "José da Silva", 0.0f, 1000.0f);
